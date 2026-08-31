fc259646f4d91888862b072f3049ea7618b68342
chips <keaidehuangpi@163.com>
2026-08-31 21:13:41 +0800

    Revert "fixxxx"
    This reverts commit dd390643359bd0051fa47272067847f82403e298.

--- src/main/java/com/odtheking/mixin/mixins/MinecraftMixin.java
+++ src/main/java/com/odtheking/mixin/mixins/MinecraftMixin.java
@@ -3,12 +3,9 @@ package com.odtheking.mixin.mixins;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.odtheking.odin.events.BlockInteractEvent;
 import com.odtheking.odin.events.EntityInteractEvent;
-import com.odtheking.odin.features.impl.combat.ModuleSwordBlock;
 import com.odtheking.odin.features.impl.boss.TerminalSolver;
 import com.odtheking.odin.utils.skyblock.dungeon.terminals.TerminalUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
@@ -25,30 +22,6 @@ public abstract class MinecraftMixin {
     @Shadow
     @Nullable
     public HitResult hitResult;
-
-    /**
-     * While an item is being used (the shield case), vanilla consumes attack
-     * clicks without entering startAttack(), so no swing state is created for
-     * the first-person renderer. Turn that consumed click into a client-side
-     * main-hand swing for SwordBlock. ClientLevel swings do not send packets.
-     */
-    @ModifyExpressionValue(
-            method = "handleKeybinds",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/KeyMapping;consumeClick()Z",
-                    ordinal = 13
-            )
-    )
-    private boolean swordBlockVisualSwing(boolean clicked) {
-        if (!clicked) return false;
-
-        LocalPlayer player = ((Minecraft) (Object) this).player;
-        if (player != null && ModuleSwordBlock.shouldAnimateSwordBlock(player)) {
-            player.swing(InteractionHand.MAIN_HAND);
-        }
-        return true;
-    }
 
     @Inject(method = "startUseItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;useItemOn(Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;"), cancellable = true)
     private void cancelBlockUse(CallbackInfo ci) {
